666987d0500e9ab8d65642cf9cdaeffa07e3224d
Brandon Harris <bharris@allegient.com>
2017-02-06 14:43:31 -0600

    Gulp - Add clean task & del dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+
+// Include Our Plugins
 var Server = require('karma').Server;
+var del = require('del');
 
 /**
  * Run test once and exit
@@ -9,4 +12,11 @@ gulp.task('test', function (done) {
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
   }, done).start();
+});
+
+gulp.task('clean', function () {
+  return del([
+    // clean all js files in app folder
+    'app/**/*.js'
+  ]);
 });
