341bc6f6f1fa2b15e10d4a3f47654b56b12a97ab
Brandon Harris <bharris@allegient.com>
2017-02-09 11:28:11 -0600

    Heroes Component Unit Test - Upgrade to 2.4

--- app/heroes.component.spec.ts
+++ app/heroes.component.spec.ts
@@ -1,135 +1,97 @@
-// import {
-//     it,
-//     describe,
-//     expect,
-//     inject,
-//     beforeEach,
-//     beforeEachProviders
-// } from '@angular/core/testing';
-// import { TestComponentBuilder } from '@angular/compiler/testing';
-// import { provide, ApplicationRef } from '@angular/core';
-// import { Router, ROUTER_PRIMARY_COMPONENT, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-// import { APP_BASE_HREF, Location } from '@angular/common';
-// import { MockApplicationRef } from '@angular/core/testing';
-// import { SpyLocation } from '@angular/common/testing/location_mock';
-// import { HTTP_PROVIDERS } from '@angular/http';
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Router } from '@angular/router';
 
-// import { HeroesComponent } from './heroes.component';
-// import { HeroService } from './hero.service';
-// import { MockHeroService } from './Testing/mocks';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { MockHeroService } from './Testing/mocks';
 
-// describe('HeroesComponent', () => {
-//     let service;
-//     let testingComponent: HeroesComponent;
-//     let router: Router;
-//     let tcb;
+describe('HeroesComponent', () => {
+    let router;
 
-//     beforeEachProviders(() => [
-//         HTTP_PROVIDERS,
-//         ROUTER_PROVIDERS,
-//         provide(Location, { useClass: SpyLocation }),
-//         provide(APP_BASE_HREF, { useValue: '/' }),
-//         provide(ROUTER_PRIMARY_COMPONENT, { useValue: HeroesComponent }),
-//         provide(ApplicationRef, { useClass: MockApplicationRef }),
-//         provide(Router, { useValue: jasmine.createSpyObj('Router', ['navigate']) }),
-//         provide(HeroService, { useClass: MockHeroService }),
-//         MockHeroService,
-//         HeroesComponent,
-//         TestComponentBuilder,
-//         HeroService
-//     ]);
+    beforeEach(() => {
+        router = {
+            navigate: jasmine.createSpy('navigate')
+        };
 
-//     beforeEach(inject([HeroesComponent, Router, MockHeroService, TestComponentBuilder],
-//         (c, r, s, _tcb) => {
-//             testingComponent = c;
-//             service = s;
-//             router = r;
-//             tcb = _tcb;
-//         })
-//     );
+        TestBed.configureTestingModule({
+            declarations: [HeroesComponent], // declare the test component
+            imports: [FormsModule, HttpModule],
+            providers: [
+                { provide: HeroService, useClass: MockHeroService },
+                { provide: Router, useValue: router }
+        ]});
+    });
 
-//     it('Should display Heroes upon init', done => {
-//         tcb.overrideProviders(HeroesComponent,
-//             [
-//                 provide(HeroService, { useClass: MockHeroService })
-//             ])
-//             .createAsync(HeroesComponent).then(fixture => {
-//                 fixture.detectChanges();
-//                 let nativeElement = fixture.nativeElement;
-//                 let component = fixture.componentInstance;
+    it('Should display Heroes upon init', async(() => {
+        TestBed.compileComponents().then(() => {
+            const fixture = TestBed.createComponent(HeroesComponent);
+            fixture.detectChanges();
+            let nativeElement = fixture.nativeElement;
+            let component = fixture.componentInstance;
 
-//                 let initTest = () => {
-//                     expect(component.heroes).toBeDefined();
-//                     expect(component.heroes.length).toBe(10);
-//                     let heroItems = nativeElement.querySelectorAll('ul.heroes li');
-//                     expect(heroItems).toBeDefined();
-//                     expect(heroItems.length).toBe(10);
-//                     done();
-//                 };
+            let initTest = () => {
+                expect(component.heroes).toBeDefined();
+                expect(component.heroes.length).toBe(10);
+                let heroItems = nativeElement.querySelectorAll('ul.heroes li');
+                expect(heroItems).toBeDefined();
+                expect(heroItems.length).toBe(10);
+            };
 
-//                 component.ngOnInit();
-//                 fixture.detectChanges();
+            component.ngOnInit();
+            fixture.detectChanges();
 
-//                 setTimeout(initTest);
-//             })
-//             .catch(e => done.fail(e));
-//     });
+            setTimeout(initTest);
+        });
+    }));
 
-//     it('Clicking Hero should display detail section', done => {
-//         tcb.overrideProviders(HeroesComponent,
-//             [
-//                 provide(HeroService, { useClass: MockHeroService })
-//             ])
-//             .createAsync(HeroesComponent).then(fixture => {
-//                 fixture.detectChanges();
-//                 let nativeElement = fixture.nativeElement;
-//                 let component = fixture.componentInstance;
+    it('Clicking Hero should display detail section', async(() => {
+        TestBed.compileComponents().then(() => {
+            const fixture = TestBed.createComponent(HeroesComponent);
+            fixture.detectChanges();
+            let nativeElement = fixture.nativeElement;
+            let component = fixture.componentInstance;
 
-//                 let test = () => {
-//                     fixture.detectChanges();
-//                     let selectedHeroText = nativeElement.querySelector('h2[name=selectedHeroText]');
-//                     expect(component.selectedHero).toBeDefined();
-//                     expect(selectedHeroText.innerText).toContain(component.selectedHero.name.toUpperCase());
-//                     let detailsButton = nativeElement.querySelector('button[name=viewDetails]');
-//                     expect(detailsButton).toBeDefined();
-//                     done();
-//                 };
+            let test = () => {
+                fixture.detectChanges();
+                let selectedHeroText = nativeElement.querySelector('h2[name=selectedHeroText]');
+                expect(component.selectedHero).toBeDefined();
+                expect(selectedHeroText.innerText).toContain(component.selectedHero.name.toUpperCase());
+                let detailsButton = nativeElement.querySelector('button[name=viewDetails]');
+                expect(detailsButton).toBeDefined();
+            };
 
-//                 component.ngOnInit();
-//                 fixture.detectChanges();
-//                 let heroItems = nativeElement.querySelectorAll('ul.heroes li');
-//                 heroItems[0].click();
+            component.ngOnInit();
+            fixture.detectChanges();
+            let heroItems = nativeElement.querySelectorAll('ul.heroes li');
+            heroItems[0].click();
 
-//                 setTimeout(test);
-//             })
-//             .catch(e => done.fail(e));
-//     });
-//     it('Should navigate to Details upon clicking Details button', done => {
-//         tcb.overrideProviders(HeroesComponent,
-//             [
-//                 provide(HeroService, { useClass: MockHeroService })
-//             ])
-//             .createAsync(HeroesComponent).then(fixture => {
-//                 fixture.detectChanges();
-//                 let nativeElement = fixture.nativeElement;
-//                 let component = fixture.componentInstance;
+            setTimeout(test);
+        });
+    }));
 
-//                 let test = () => {
-//                     fixture.detectChanges();
-//                     expect(router.navigate).toHaveBeenCalledWith(['HeroDetail', { id: +component.selectedHero.id }]);
-//                     done();
-//                 };
+    it('Should navigate to Details upon clicking Details button', async(() => {
+        TestBed.compileComponents().then(() => {
+            const fixture = TestBed.createComponent(HeroesComponent);
+            fixture.detectChanges();
+            let nativeElement = fixture.nativeElement;
+            let component = fixture.componentInstance;
 
-//                 component.ngOnInit();
-//                 fixture.detectChanges();
-//                 let heroItems = nativeElement.querySelectorAll('ul.heroes li');
-//                 heroItems[0].click();
-//                 fixture.detectChanges();
-//                 let detailsButton = nativeElement.querySelector('button[name=viewDetails]');
-//                 detailsButton.click();
+            let test = () => {
+                fixture.detectChanges();
+                expect(router.navigate).toHaveBeenCalledWith(['/detail', +component.selectedHero.id]);
+            };
 
-//                 setTimeout(test);
-//             })
-//             .catch(e => done.fail(e));
-//     });
-// });
+            component.ngOnInit();
+            fixture.detectChanges();
+            let heroItems = nativeElement.querySelectorAll('ul.heroes li');
+            heroItems[0].click();
+            fixture.detectChanges();
+            let detailsButton = nativeElement.querySelector('button[name=viewDetails]');
+            detailsButton.click();
+
+            setTimeout(test);
+        });
+    }));
+});
